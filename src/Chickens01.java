ac311e94d50a08dfd4a1f26229341079ef76265b
ALUNO <ALUNO@LAB04-PC36>
2024-06-05 19:26:08 -0300

    Commit project chicken01

---
+++ src/Chickens01.java
@@ -0,0 +1,26 @@
+
+
+public class Chickens01 {
+    public static void main(String[] args) {
+
+//3-2 Exercicio:
+//  1° Parte: A galinha bota 5 ovos e ele tem tres galinha depois ganhou uma e depois perdeu a metade.
+//  Segunda 5x3=15
+//  Terça   5x4=20
+//  Quarta  5x2=10 "Total" igual á 45.
+int x = 5;//Valor dos ovos
+System.out.println("Ovos sao " + x);
+x = 5 * 3;
+System.out.println("Segunda Feira " + x);
+x = 5 * 4 ;
+System.out.println("Terca Feira " + x);
+x = 5 * 2 ;
+System.out.println("Quarta Feira " + x);
+
+System.out.println("Somando 15+20+10");
+
+x = 15+20+10 ;
+System.out.println("O total sera " +x);
+
+    }
+}
